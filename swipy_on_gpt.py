36b0261dac45228385fa0013ff79cd94ccf14511
Oleksandr Tereshchenko <toporok@gmail.com>
2023-01-29 17:52:33 +0200

    call gpt

--- swipy_on_gpt.py
+++ swipy_on_gpt.py
@@ -2,6 +2,7 @@
 import logging
 import os
 
+import openai
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
@@ -9,6 +10,7 @@ from telegram.ext.filters import User, TEXT
 
 load_dotenv()
 
+openai.api_key = os.environ["OPENAI_API_KEY"]
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 ALLOWED_USERS = os.environ["ALLOWED_USERS"].split(",")
 
@@ -17,12 +19,15 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # noinspection PyUnusedLocal
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.effective_chat.send_message(text="I'm a bot, please talk to me!")
+    await update.effective_chat.send_message(text="Hey Vsauce, Swipy here!")
 
 
 # noinspection PyUnusedLocal
 async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.effective_chat.send_message(text=update.effective_message.text[::-1])
+    # TODO oleksandr: replace with "text-davinci-003"
+    gpt_response = await openai.Completion.acreate(prompt=update.effective_message.text, engine="text-ada-001")
+    for choice in gpt_response.choices:
+        await update.effective_chat.send_message(text=choice.text, parse_mode="Markdown")
 
 
 if __name__ == "__main__":
