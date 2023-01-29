90e7aba41ec6e962e2472b73b54202374377ccbe
Oleksandr Tereshchenko <toporok@gmail.com>
2023-01-29 15:31:49 +0200

    swipy_on_gpt.py

--- swipy_on_gpt.py
+++ swipy_on_gpt.py
@@ -1,22 +1,27 @@
 import logging
 import os
 
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext.filters import User
+
+load_dotenv()
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  # pylint: disable=unused-argument
+    await update.effective_chat.send_message(text="I'm a bot, please talk to me!")
 
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token("TOKEN").build()
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    start_handler = CommandHandler("start", start)
+    # make handlers work only for specific user_ids
+    start_handler = CommandHandler("start", start, filters=User(username="@teremterem"))
     application.add_handler(start_handler)
 
     application.run_polling()
