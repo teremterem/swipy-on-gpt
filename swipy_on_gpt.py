06a7888061edee0b1b3489d34bf9178b6fe220ca
Oleksandr Tereshchenko <toporok@gmail.com>
2023-01-29 16:01:12 +0200

    swipy_on_gpt.py

--- swipy_on_gpt.py
+++ swipy_on_gpt.py
@@ -1,10 +1,11 @@
+# pylint: disable=unused-argument
 import logging
 import os
 
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from telegram.ext.filters import User
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+from telegram.ext.filters import User, TEXT
 
 load_dotenv()
 
@@ -14,15 +15,21 @@ ALLOWED_USERS = os.environ["ALLOWED_USERS"].split(",")
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  # pylint: disable=unused-argument
+# noinspection PyUnusedLocal
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.effective_chat.send_message(text="I'm a bot, please talk to me!")
+
+
+# noinspection PyUnusedLocal
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.effective_chat.send_message(text="GPT-3 ?")
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     allowed_users_filter = User(username=ALLOWED_USERS)
 
-    start_handler = CommandHandler("start", start, filters=allowed_users_filter)
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler("start", start, filters=allowed_users_filter))
+    application.add_handler(MessageHandler(TEXT, gpt))
 
     application.run_polling()
