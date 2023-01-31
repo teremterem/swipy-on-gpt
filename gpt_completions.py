1606b71df396173900c63ed9d924f664a138dad0
Oleksandr Tereshchenko <toporok@gmail.com>
2023-01-31 11:47:53 +0200

    Gpt completion classes (#4)
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes
    
    * gpt completion classes

--- gpt_completions.py
+++ gpt_completions.py
@@ -1,66 +1,116 @@
-import itertools
+# pylint: disable=too-few-public-methods
 import random
+from typing import Collection
 
 import openai
-from telegram import Update
-from telegram.constants import ParseMode
 
 from swipy_config import MOCK_GPT
 
 
-class SimpleGptCompletion:
-    def __init__(self, temperature: float = 1):
+class GptCompletion:
+    def __init__(self, prompt: str, temperature: float, stop_list: Collection[str]):
+        self.prompt = prompt
         self.temperature = temperature
+        self.completion: str | None = None
+        self.stop_list = stop_list
 
-    def create_prompt(self, update: Update) -> str:
-        return update.effective_message.text
-
-    async def display_gpt_completion(self, update: Update) -> None:
-        prompt = self.create_prompt(update)
-        completion = await self.request_gpt_completion(prompt, update)
-        message = f"==== {self.display_model_name()} ====\n\n{prompt}{completion}"
-        await update.effective_chat.send_message(text=message, parse_mode=ParseMode.MARKDOWN)
-
-    def display_model_name(self) -> str:
-        return f"{self.__class__.__name__} T={self.temperature}"
-
-    async def request_gpt_completion(self, prompt: str, update: Update) -> str:
-        await update.effective_chat.send_message(text=prompt)
+    async def fulfil(self) -> None:
         if MOCK_GPT:
-            response_text = f"\n\nhErE gOeS gPt ReSpOnSe  (iT's a mOCK!) {random.randint(0, 1000000)}"
+            self.completion = f"\n\nhErE gOeS gPt ReSpOnSe  (iT's a mOCK!) {random.randint(0, 1000000)}"
         else:
             gpt_response = await openai.Completion.acreate(
-                prompt=prompt,
+                prompt=self.prompt,
                 engine="text-davinci-003",
                 temperature=self.temperature,
-                max_tokens=256,
-                # TODO oleksandr: stop=["\n", " Human:", " AI:"],
+                max_tokens=512,
+                stop=self.stop_list,
             )
+            self.completion = gpt_response.choices[0].text
             assert len(gpt_response.choices) == 1, f"Expected only one gpt choice, but got {len(gpt_response.choices)}"
-            response_text = gpt_response.choices[0].text
-        await update.effective_chat.send_message(text=repr(response_text))
-
-        return response_text
 
 
-class DialogGptCompletion(SimpleGptCompletion):
-    def __init__(self, temperature: float = 1):
-        super().__init__(temperature=temperature)
-        self.dialog_history = []
+class PaddedGptCompletion(GptCompletion):
+    def __init__(self, prompt_content: str, prompt_template: str, temperature: float, stop_list: Collection[str]):
+        prompt = prompt_template.format(prompt_content)
+        super().__init__(prompt=prompt, temperature=temperature, stop_list=stop_list)
 
-    def create_prompt(self, update: Update) -> str:
-        self.dialog_history.append(" ".join([self.create_user_name(update), update.effective_message.text]))
-        prompt = "\n".join(itertools.chain(self.dialog_history, [self.create_bot_name()]))
-        return prompt
 
-    def create_user_name(self, update: Update) -> str:
-        return f" *{update.effective_user.first_name}:*"
+class DialogGptCompletion(PaddedGptCompletion):  # pylint: disable=too-many-instance-attributes
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        user_name: str,
+        bot_name: str,
+        user_utterance: str,
+        prompt_template: str,
+        temperature: float,
+        previous_completion: "DialogGptCompletion" = None,
+    ):
+        self.user_name = user_name
+        self.bot_name = bot_name
+        self.user_utterance = user_utterance
+        self.previous_completion = previous_completion
 
-    def create_bot_name(self) -> str:
-        return " *Swipy:*"
+        self.user_prefix = self.utterance_prefix(self.user_name)
+        self.bot_prefix = self.utterance_prefix(self.bot_name)
 
-    async def request_gpt_completion(self, prompt: str, update: Update) -> str:
-        completion = await super().request_gpt_completion(prompt, update)
-        completion = completion.strip()
-        self.dialog_history.append(" ".join([self.create_bot_name(), completion.strip()]))
-        return " " + completion
+        prompt_parts = []
+        self.build_prompt_content(prompt_parts)
+        prompt_parts.append(self.bot_prefix)
+        prompt_content = "\n".join(prompt_parts)
+        super().__init__(
+            prompt_template=prompt_template,
+            prompt_content=prompt_content,
+            temperature=temperature,
+            stop_list=[
+                # "\n",  # TODO oleksandr: enable this if it's the very first exchange ?
+                self.user_prefix,
+                # self.bot_prefix,
+            ],
+        )
+
+        self.completion_before_strip: str | None = None
+
+    def build_prompt_content(self, prompt_parts: list[str]) -> None:
+        if self.previous_completion is not None:
+            self.previous_completion.build_prompt_content(prompt_parts)
+            if self.previous_completion.completion is not None:
+                prompt_parts.append(f"{self.bot_prefix} {self.previous_completion.completion}")
+
+        if self.user_utterance is not None:
+            prompt_parts.append(f"{self.user_prefix} {self.user_utterance}")
+
+    def utterance_prefix(self, utterer_name) -> str:
+        return f"*{utterer_name}*:"
+
+    async def fulfil(self) -> None:
+        await super().fulfil()
+        self.completion_before_strip = self.completion
+        self.completion = self.completion.strip()
+
+
+class DialogGptCompletionHistory:
+    def __init__(self, bot_name: str, experiment_name, prompt_template: str = "{}", temperature: float = 1):
+        self.bot_name = bot_name
+        self.experiment_name = experiment_name
+        self.prompt_template = prompt_template
+        self.temperature = temperature
+
+        self.completions: list[DialogGptCompletion] = []
+
+    def new_user_utterance(self, user_name: str, user_utterance: str) -> DialogGptCompletion:
+        gpt_completion = DialogGptCompletion(
+            prompt_template=self.prompt_template,
+            user_name=user_name,
+            bot_name=self.bot_name,
+            user_utterance=user_utterance,
+            previous_completion=self.completions[-1] if self.completions else None,
+            temperature=self.temperature,
+        )
+        self.completions.append(gpt_completion)
+        return gpt_completion
+
+    def clear_history(self) -> None:
+        self.completions = []
+
+    def __str__(self) -> str:
+        return f"{self.experiment_name} T={self.temperature:.1f}"
--- swipy_on_gpt.py
+++ swipy_on_gpt.py
@@ -1,46 +1,75 @@
-# pylint: disable=unused-argument
+# pylint: disable=unused-argument,global-statement
 import asyncio
 
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 from telegram.ext.filters import User, TEXT
 
-from gpt_completions import SimpleGptCompletion, DialogGptCompletion
+from gpt_completions import DialogGptCompletionHistory
 from swipy_config import TELEGRAM_TOKEN, ALLOWED_USERS
 
-SIMPLE_GPT_COMP_T0: SimpleGptCompletion
-SIMPLE_GPT_COMP_T1: SimpleGptCompletion
-DIALOG_GPT_COMP_T0: DialogGptCompletion
-DIALOG_GPT_COMP_T1: DialogGptCompletion
+BOT_NAME = "Swipy"
+# TODO oleksandr: unhardcode the names
+CURIOS_PROMPT = (
+    "Swipy is a chatbot that uses GPT-3 to answer questions from a user by the name Oleksandr. "
+    "Swipy is genuinely curious and wants to learn more about Oleksandr. For that reason it asks Oleksandr followup "
+    "questions to clarify every detail that is not immediately clear to Swipy in whatever Oleksandr is telling "
+    "Swipy. Below is a conversation between Swipy and Oleksandr.\n\n"
+)
 
-
-def init_gpt_completions() -> None:
-    # pylint: disable=global-statement
-    global SIMPLE_GPT_COMP_T0, SIMPLE_GPT_COMP_T1, DIALOG_GPT_COMP_T0, DIALOG_GPT_COMP_T1
-    SIMPLE_GPT_COMP_T0 = SimpleGptCompletion(temperature=0)
-    SIMPLE_GPT_COMP_T1 = SimpleGptCompletion(temperature=1)
-    DIALOG_GPT_COMP_T0 = DialogGptCompletion(temperature=0)
-    DIALOG_GPT_COMP_T1 = DialogGptCompletion(temperature=1)
-
-
-init_gpt_completions()
+NO_PROMPT_T0 = DialogGptCompletionHistory(
+    bot_name=BOT_NAME,
+    experiment_name="NO PROMPT",
+    temperature=0,
+)
+NO_PROMPT_T1 = DialogGptCompletionHistory(
+    bot_name=BOT_NAME,
+    experiment_name="NO PROMPT",
+    temperature=1,
+)
+CURIOUS_PROMPT_T0 = DialogGptCompletionHistory(
+    bot_name=BOT_NAME,
+    experiment_name="CURIOUS PROMPT",
+    prompt_template=CURIOS_PROMPT + "{}",
+    temperature=0,
+)
+CURIOUS_PROMPT_T1 = DialogGptCompletionHistory(
+    bot_name=BOT_NAME,
+    experiment_name="CURIOUS PROMPT",
+    prompt_template=CURIOS_PROMPT + "{}",
+    temperature=1,
+)
 
 
 # noinspection PyUnusedLocal
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    init_gpt_completions()
-    await update.effective_chat.send_message(text="Hey Vsauce, Swipy here!")
+    NO_PROMPT_T0.clear_history()
+    NO_PROMPT_T1.clear_history()
+
+    # TODO oleksandr: all utterances (even hardcoded ones) should always be visible to GPT-3
+    await update.effective_chat.send_message(text=f"Hey Vsauce, {BOT_NAME} here!")
+
+
+async def reply_with_gpt_completion(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, history: DialogGptCompletionHistory
+) -> None:
+    gpt_completion = history.new_user_utterance(update.effective_user.first_name, update.effective_message.text)
+    await asyncio.sleep(0.1)
+    await update.effective_chat.send_message(
+        text=f"{str(history).upper()}\n\n" f"{gpt_completion.prompt}",
+        parse_mode=ParseMode.MARKDOWN,
+    )
+    await gpt_completion.fulfil()
+    await update.effective_chat.send_message(text=gpt_completion.completion, parse_mode=ParseMode.MARKDOWN)
 
 
 # noinspection PyUnusedLocal
 async def reply_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # await SIMPLE_GPT_COMP_T0.display_gpt_completion(update)
-    # await asyncio.sleep(1)
-    # await SIMPLE_GPT_COMP_T1.display_gpt_completion(update)
-    # await asyncio.sleep(1)
-    await DIALOG_GPT_COMP_T0.display_gpt_completion(update)
-    await asyncio.sleep(1)
-    await DIALOG_GPT_COMP_T1.display_gpt_completion(update)
+    await reply_with_gpt_completion(update, context, NO_PROMPT_T0)
+    await reply_with_gpt_completion(update, context, NO_PROMPT_T1)
+    await reply_with_gpt_completion(update, context, CURIOUS_PROMPT_T0)
+    await reply_with_gpt_completion(update, context, CURIOUS_PROMPT_T1)
 
 
 def main() -> None:
