7644f7c0d1096bc09f8fecd27ef340e00f00c63e
Oleksandr Tereshchenko <toporok@gmail.com>
2023-02-01 19:59:58 +0200

    Show "typing" (#6)
    * typing
    
    * typing
    
    * VNED-7119
    
    * different prompt
    
    * different prompt
    
    * buttons
    
    * buttons

--- swipy_on_gpt.py
+++ swipy_on_gpt.py
@@ -1,8 +1,8 @@
-# pylint: disable=unused-argument,global-statement
+# pylint: disable=unused-argument
 import asyncio
 
 from telegram import Update
-from telegram.constants import ParseMode
+from telegram.constants import ParseMode, ChatAction
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 from telegram.ext.filters import User, TEXT
 
@@ -11,42 +11,23 @@ from swipy_config import TELEGRAM_TOKEN, ALLOWED_USERS
 
 BOT_NAME = "Swipy"
 # TODO oleksandr: unhardcode the names
-CURIOS_PROMPT = (
-    "Swipy is a chatbot that uses GPT-3 to answer questions from a user by the name Oleksandr. "
-    "Swipy is genuinely curious and wants to learn more about Oleksandr. For that reason it asks Oleksandr followup "
-    "questions to clarify every detail that is not immediately clear to Swipy in whatever Oleksandr is telling "
-    "Swipy. Below is a conversation between Swipy and Oleksandr.\n\n"
+FOLLOWUP_PROMPT = (
+    "Your name is Swipy and the user's name is Oleksandr. Here is your dialog with Oleksandr. If Oleksandr mentions "
+    "any people, things, places, events etc. you don't know about (or if you don't know any details about mentioned "
+    "people, things, places, events etc. in relation to Oleksandr specifically) then follow up with corresponding "
+    "clarifying questions to Oleksandr.\n\n"
 )
 
-NO_PROMPT_T0 = DialogGptCompletionHistory(
+FOLLOWUP_PROMPT_T1 = DialogGptCompletionHistory(
     bot_name=BOT_NAME,
-    experiment_name="NO PROMPT",
-    temperature=0,
-)
-NO_PROMPT_T1 = DialogGptCompletionHistory(
-    bot_name=BOT_NAME,
-    experiment_name="NO PROMPT",
-    temperature=1,
-)
-CURIOUS_PROMPT_T0 = DialogGptCompletionHistory(
-    bot_name=BOT_NAME,
-    experiment_name="CURIOUS PROMPT",
-    prompt_template=CURIOS_PROMPT + "{}",
-    temperature=0,
-)
-CURIOUS_PROMPT_T1 = DialogGptCompletionHistory(
-    bot_name=BOT_NAME,
-    experiment_name="CURIOUS PROMPT",
-    prompt_template=CURIOS_PROMPT + "{}",
+    experiment_name="FOLLOWUP PROMPT",
+    prompt_template=FOLLOWUP_PROMPT + "{}",
     temperature=1,
 )
 
 
 # noinspection PyUnusedLocal
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    NO_PROMPT_T0.clear_history()
-    NO_PROMPT_T1.clear_history()
-
     # TODO oleksandr: all utterances (even hardcoded ones) should always be visible to GPT-3
     await update.effective_chat.send_message(text=f"Hey Vsauce, {BOT_NAME} here!")
 
@@ -55,21 +36,42 @@ async def reply_with_gpt_completion(
     update: Update, context: ContextTypes.DEFAULT_TYPE, history: DialogGptCompletionHistory
 ) -> None:
     gpt_completion = history.new_user_utterance(update.effective_user.first_name, update.effective_message.text)
-    await asyncio.sleep(0.1)
-    await update.effective_chat.send_message(
-        text=f"{str(history).upper()}\n\n" f"{gpt_completion.prompt}",
-        parse_mode=ParseMode.MARKDOWN,
-    )
+
+    keep_typing = True
+
+    async def keep_typing_task():
+        while keep_typing:
+            await update.effective_chat.send_chat_action(ChatAction.TYPING)
+            await asyncio.sleep(10)
+
+    await asyncio.sleep(1)
+    asyncio.get_event_loop().create_task(keep_typing_task())
+
+    # await update.effective_chat.send_message(
+    #     text=f"{str(history).upper()}\n\n" f"{gpt_completion.prompt}",
+    #     parse_mode=ParseMode.MARKDOWN,
+    # )
     await gpt_completion.fulfil()
-    await update.effective_chat.send_message(text=gpt_completion.completion, parse_mode=ParseMode.MARKDOWN)
+    keep_typing = False
+
+    # add a button to the message
+    await update.effective_chat.send_message(
+        text=gpt_completion.completion,
+        parse_mode=ParseMode.MARKDOWN,
+        # reply_markup=InlineKeyboardMarkup(
+        #     [
+        #         [
+        #             InlineKeyboardButton(text="👍", callback_data="like"),
+        #             InlineKeyboardButton(text="👎", callback_data="dislike"),
+        #         ]
+        #     ],
+        # ),
+    )
 
 
 # noinspection PyUnusedLocal
 async def reply_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await reply_with_gpt_completion(update, context, NO_PROMPT_T0)
-    await reply_with_gpt_completion(update, context, NO_PROMPT_T1)
-    await reply_with_gpt_completion(update, context, CURIOUS_PROMPT_T0)
-    await reply_with_gpt_completion(update, context, CURIOUS_PROMPT_T1)
+    await reply_with_gpt_completion(update, context, FOLLOWUP_PROMPT_T1)
 
 
 def main() -> None:
