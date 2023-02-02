55688d5464e69c316749b03ac5614b87f500f3a3
Oleksandr Tereshchenko <toporok@gmail.com>
2023-02-02 21:17:35 +0200

    django

---
+++ swipy_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SwipyAppConfig(AppConfig):  # pylint: disable=too-few-public-methods
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "swipy_app"
