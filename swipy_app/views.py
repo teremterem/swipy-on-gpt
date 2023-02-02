27b226854b3372ba5e77bb6db9730b169a995806
Oleksandr Tereshchenko <toporok@gmail.com>
2023-02-02 22:07:31 +0200

    django async

--- swipy_app/views.py
+++ swipy_app/views.py
@@ -1,8 +1,5 @@
-import logging
-
 from django.http import HttpResponse, HttpRequest
 
 
 async def index(request: HttpRequest) -> HttpResponse:  # pylint: disable=unused-argument
-    logging.getLogger().debug("debug Aloha, world!")
     return HttpResponse("Hello, world!")
