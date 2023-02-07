ddfcb60e5692fdb39cf236dab5527eefbb96fcd3
Oleksandr Tereshchenko <toporok@gmail.com>
2023-02-07 19:24:06 +0200

    minor

--- swipy_app/urls.py
+++ swipy_app/urls.py
@@ -4,6 +4,6 @@ from swipy_config import TELEGRAM_TOKEN
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),  # TODO oleksandr: don't expose this, hide the health check url
     path(f"{TELEGRAM_TOKEN}/", views.telegram_webhook, name="telegram_webhook"),
 ]
