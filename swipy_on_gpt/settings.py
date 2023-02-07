7dbe0861c781378bf550ca7d9cd2530e7b6c4cd3
Oleksandr Tereshchenko <toporok@gmail.com>
2023-02-07 21:20:08 +0200

    nginx

--- swipy_on_gpt/settings.py
+++ swipy_on_gpt/settings.py
@@ -29,6 +29,7 @@ DEBUG = True  # TODO oleksandr: get this from the environment; make False the de
 ALLOWED_HOSTS = [
     SWIPY_DJANGO_HOST,
     "localhost",
+    "swipy_server",  # TODO oleksandr: get rid of this ?
 ]
 
 # Application definition
