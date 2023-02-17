1e9a7c5180f1d3c16f67696ea6f8f20a3577de64
Oleksandr Tereshchenko <toporok@gmail.com>
2023-02-17 23:25:22 +0200

    long term memory v1

--- swipy_app/models.py
+++ swipy_app/models.py
@@ -31,4 +31,5 @@ class Utterance(models.Model):
     name = models.TextField()
     text = models.TextField()
     is_bot = models.BooleanField()
+    is_end_of_conv = models.BooleanField(default=False)
     gpt_completion = models.ForeignKey(GptCompletion, on_delete=models.CASCADE, null=True)
