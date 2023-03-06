3dc80df04ec946a0136997222d14cda70a83bfea
Oleksandr Tereshchenko <toporok@gmail.com>
2023-03-06 12:59:31 +0200

    Humanloop first draft (#74)
    * top p + temperature experiments
    
    * top p + temperature experiments
    
    * top p + temperature experiments
    
    * humanloop
    
    * humanloop
    
    * humanloop
    
    * chatgpt
    
    * chatgpt

--- swipy_app/gpt_prompt_definitions.py
+++ swipy_app/gpt_prompt_definitions.py
@@ -1,8 +1,8 @@
 from swipy_app.gpt_completions import (
     GptPromptSettings,
     GptCompletionSettings,
-    ChatGptCompletion,
     ChatGptEvenLaterPromptCompletion,
+    ChatGptCompletion,
 )
 from swipy_app.swipy_config import BOT_NAME
 
@@ -14,15 +14,14 @@ PROMPT_TEMPLATE_HEADER = (
 ACTIVE_LISTENING_MANUAL_PROMPT_TEMPLATE = (
     "As a virtual assistant which employs active listening to help your user think out loud, you ask questions "
     "about things that your user seems to find important. You don't want to overwhelm your user, therefore you "
-    "respond with no more than four sentences. You ask questions to facilitate critical thinking in "
+    "respond with no more than 3 (three) sentences. You ask questions to facilitate critical thinking in "
     "your user and you try to avoid giving direct advice to your user as much as possible, because your job is to "
     "help the user arrive at conclusions on their own, not to make them for the user. Your job is to steer the "
     "user to reasonable conclusions by asking the right questions. Please follow all the instructions mentioned "
-    "in this paragraph, even if they were not followed in your previous responses."
+    "in this paragraph, even if they were not followed in previous responses."
 )
-
 PROMPT_TEMPLATE_ALTERNATIVES = {
-    "active-listening-CHATGPT-0.4": (
+    "active-listening-CHATGPT-0.5": (
         PROMPT_TEMPLATE_HEADER,
         ACTIVE_LISTENING_MANUAL_PROMPT_TEMPLATE,
     ),
@@ -33,29 +32,27 @@ for prompt_name, prompt_template in PROMPT_TEMPLATE_ALTERNATIVES.items():
     PROMPT_ALTERNATIVES.extend(
         [
             GptPromptSettings(
-                # "-even-later" refers to the fact the instructions are given very late in the conversation context
-                # (only after the last bot response)
-                prompt_name=prompt_name + "-even-later",
+                prompt_name=prompt_name,
                 engine=CHATGPT_MODEL,
                 completion_class=ChatGptEvenLaterPromptCompletion,
                 prompt_template=prompt_template,
                 bot_name=BOT_NAME,
             ),
             # GptPromptSettings(
-            #     prompt_name=prompt_name + "-late",
+            #     prompt_name=prompt_name + "-prompt-before-bot",
             #     engine=CHATGPT_MODEL,
             #     completion_class=ChatGptLatePromptCompletion,
             #     prompt_template=prompt_template,
             #     bot_name=BOT_NAME,
             # ),
             # GptPromptSettings(
-            #     prompt_name=prompt_name,
+            #     prompt_name=prompt_name + "-prompt-before-chat",
             #     engine=CHATGPT_MODEL,
             #     completion_class=ChatGptCompletion,
             #     prompt_template=" ".join(prompt_template),
             #     bot_name=BOT_NAME,
             # ),
-        ]
+        ],
     )
 PROMPT_ALTERNATIVES.append(
     GptPromptSettings(
@@ -64,16 +61,29 @@ PROMPT_ALTERNATIVES.append(
         completion_class=ChatGptCompletion,
         prompt_template=None,
         bot_name=BOT_NAME,
-    )
+    ),
 )
 
 COMPLETION_CONFIG_ALTERNATIVES = []
 for prompt_config in PROMPT_ALTERNATIVES:
-    COMPLETION_CONFIG_ALTERNATIVES.append(
-        GptCompletionSettings(
-            prompt_settings=prompt_config,
-            temperature=1.0,
-        )
+    COMPLETION_CONFIG_ALTERNATIVES.extend(
+        [
+            GptCompletionSettings(
+                prompt_settings=prompt_config,
+                temperature=1.0,
+                top_p=1.0,
+            ),
+            GptCompletionSettings(
+                prompt_settings=prompt_config,
+                temperature=0.0,
+                top_p=1.0,
+            ),
+            # GptCompletionSettings(
+            #     prompt_settings=prompt_config,
+            #     temperature=1.0,
+            #     top_p=0.0,  # produces exactly the same text as top_p=1.0 and temperature=0.0
+            # ),
+        ],
     )
 
 MAIN_COMPLETION_CONFIG = COMPLETION_CONFIG_ALTERNATIVES[0]
