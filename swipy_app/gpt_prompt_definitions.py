960285fb9422411c0dbea3300e67826995bdab55
Oleksandr Tereshchenko <toporok@gmail.com>
2023-03-07 10:54:01 +0200

    oldest prompt

--- swipy_app/gpt_prompt_definitions.py
+++ swipy_app/gpt_prompt_definitions.py
@@ -40,7 +40,7 @@ def _generate_completion_config_alternatives(
 CHATGPT_MODEL = "gpt-3.5-turbo-0301"  # TODO oleksandr: use "gpt-3.5-turbo" instead (receives updates) ?
 DAVINCI_MODEL = "text-davinci-003"
 
-PROMPT_TEMPLATE_HEADER = (
+PROMPT_HEADER_TEMPLATE = (
     "Your name is {BOT} and the name of your user is {USER}. Below is your conversation with {USER}."
 )
 ACTIVE_LISTENING_PROMPT_TEMPLATE = (
@@ -55,13 +55,22 @@ ACTIVE_LISTENING_CHATGPT_PROMPT_TEMPLATE = (
     + " Ensure that your next message follows these instructions, even if previous messages did not."
 )
 
+ASK_EVERYTHING_0_1_PROMPT_HEADER_TEMPLATE = (
+    "Your name is {BOT} and the user's name is {USER}. Here is your dialog with {USER}."
+)
+ASK_EVERYTHING_0_1_PROMPT_TEMPLATE = (
+    "If {USER} mentions any people, things, places, events etc. you don't know about (or if you don't "
+    "know any details about mentioned people, things, places, events etc. in relation to {USER} "
+    "specifically) then follow up with corresponding clarifying questions to {USER}."
+)
+
 GEN_ALT_BUTTONS = {
     ("chatgpt_alts", "ChatGPT alternatives"): _generate_completion_config_alternatives(
         [
             GptPromptSettings(
                 prompt_name="active-listening-CHATGPT-0.8",
                 prompt_template=(
-                    PROMPT_TEMPLATE_HEADER,
+                    PROMPT_HEADER_TEMPLATE,
                     ACTIVE_LISTENING_CHATGPT_PROMPT_TEMPLATE,
                 ),
                 engine=CHATGPT_MODEL,
@@ -70,7 +79,7 @@ GEN_ALT_BUTTONS = {
             ),
         ]
     ),
-    ("chatgpt_no_prompt", 'ChatGPT "no prompt"'): _generate_completion_config_alternatives(
+    ("chatgpt_no_prompt_alts", 'ChatGPT "no prompt"'): _generate_completion_config_alternatives(
         [
             GptPromptSettings(
                 prompt_name="CHATGPT-NO-PROMPT",
@@ -87,13 +96,40 @@ GEN_ALT_BUTTONS = {
                 prompt_name="active-listening-DAVINCI-0.8",
                 prompt_template=" ".join(
                     [
-                        PROMPT_TEMPLATE_HEADER,
+                        PROMPT_HEADER_TEMPLATE,
                         ACTIVE_LISTENING_PROMPT_TEMPLATE,
                     ]
                 )
                 + "\n\n---\n\n{DIALOG}",
                 engine=DAVINCI_MODEL,
                 completion_class=TextDialogGptCompletion,
+                bot_name=BOT_NAME,
+            ),
+        ]
+    ),
+    ("old_prompt_alts", "Oldest prompt"): _generate_completion_config_alternatives(
+        [
+            GptPromptSettings(
+                prompt_name="ask-everything-0.1",
+                prompt_template=" ".join(
+                    [
+                        ASK_EVERYTHING_0_1_PROMPT_HEADER_TEMPLATE,
+                        ASK_EVERYTHING_0_1_PROMPT_TEMPLATE,
+                    ]
+                ),
+                double_newline_between_utterances=False,
+                engine=DAVINCI_MODEL,
+                completion_class=TextDialogGptCompletion,
+                bot_name=BOT_NAME,
+            ),
+            GptPromptSettings(
+                prompt_name="ask-everything-CHATGPT-0.1",
+                prompt_template=(
+                    ASK_EVERYTHING_0_1_PROMPT_HEADER_TEMPLATE,
+                    ASK_EVERYTHING_0_1_PROMPT_TEMPLATE,
+                ),
+                engine=CHATGPT_MODEL,
+                completion_class=ChatGptEvenLaterPromptCompletion,
                 bot_name=BOT_NAME,
             ),
         ]
