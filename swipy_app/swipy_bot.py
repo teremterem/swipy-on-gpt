a9bd158c689d00bb5e53c936fe2b27178c469f9a
Oleksandr Tereshchenko <toporok@gmail.com>
2023-03-11 20:28:14 +0200

    ukr

--- swipy_app/swipy_bot.py
+++ swipy_app/swipy_bot.py
@@ -10,7 +10,7 @@ from telegram.ext.filters import TEXT
 from swipy_app.gpt_prompt_definitions import MAIN_COMPLETION_CONFIG, NO_PROMPT_COMPLETION_CONFIG
 from swipy_app.models import Utterance, TelegramUpdate, UtteranceConversation
 from swipy_app.swipy_config import TELEGRAM_TOKEN
-from swipy_app.swipy_l10n import DefaultLang
+from swipy_app.swipy_l10n import SwipyL10n, LANGUAGES, DEFAULT_LANG
 from swipy_app.swipy_utils import current_time_utc_ms
 
 # TODO oleksandr: is this a dirty hack ? use this instead ?
@@ -18,38 +18,45 @@ from swipy_app.swipy_utils import current_time_utc_ms
 UPDATE_DB_MODELS_VOLATILE_CACHE: dict[int, TelegramUpdate] = {}
 
 
-def get_all_btn_set() -> set[str]:
+def get_all_btn_set(lang: SwipyL10n) -> set[str]:
     all_btn_set = {
-        DefaultLang.BTN_I_JUST_WANT_TO_CHAT,
-        DefaultLang.BTN_SMTH_IS_BOTHERING_ME,
-        DefaultLang.BTN_HELP_ME_FIGHT_PROCRAST,
-        DefaultLang.BTN_SOMETHING_ELSE,
-        DefaultLang.BTN_MAIN_MENU,
-        DefaultLang.BTN_EXPAND_ON_THIS,
-        DefaultLang.BTN_THANKS,
-        DefaultLang.BTN_NOT_HELPFUL,
-        DefaultLang.BTN_PROCEED_WITH_SUBJECT,
+        lang.BTN_I_JUST_WANT_TO_CHAT,
+        lang.BTN_SMTH_IS_BOTHERING_ME,
+        lang.BTN_HELP_ME_FIGHT_PROCRAST,
+        lang.BTN_SOMETHING_ELSE,
+        lang.BTN_LANGUAGE,
+        lang.BTN_MAIN_MENU,
+        lang.BTN_EXPAND_ON_THIS,
+        lang.BTN_THANKS,
+        lang.BTN_NOT_HELPFUL,
+        lang.BTN_PROCEED_WITH_SUBJECT,
+        lang.BTN_ENGLISH,
+        lang.BTN_UKRAINIAN,
     }
     return all_btn_set
 
 
 async def reply_with_gpt_completion(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # pylint: disable=too-many-locals,too-many-statements
-    user_requested_new_conv = update.effective_message.text == "/start"
-    any_reply_button_was_pressed = update.effective_message.text in get_all_btn_set()
-    expand_on_this_was_requested = update.effective_message.text == DefaultLang.BTN_EXPAND_ON_THIS
-    main_menu_was_requested = update.effective_message.text == DefaultLang.BTN_MAIN_MENU
-    thanks_was_requested = update.effective_message.text == DefaultLang.BTN_THANKS
+    # pylint: disable=too-many-locals,too-many-statements,too-many-branches
+    user_first_name = update.effective_user.first_name  # TODO oleksandr: update db user info upon every tg update ?
+    tg_update_in_db = UPDATE_DB_MODELS_VOLATILE_CACHE.pop(id(update))
+
+    # TODO oleksandr: get language from db
+    lang = LANGUAGES.get("en", DEFAULT_LANG)
+
+    start_requested = update.effective_message.text == "/start"
+    language_selected = update.effective_message.text in {lang.BTN_ENGLISH, lang.BTN_UKRAINIAN}
+    any_reply_button_was_pressed = update.effective_message.text in get_all_btn_set(lang)
+    expand_on_this_was_requested = update.effective_message.text == lang.BTN_EXPAND_ON_THIS
+    main_menu_was_requested = update.effective_message.text == lang.BTN_MAIN_MENU
+    thanks_was_requested = update.effective_message.text == lang.BTN_THANKS
 
     if expand_on_this_was_requested:
         gpt_completion_settings = NO_PROMPT_COMPLETION_CONFIG
     else:
         gpt_completion_settings = MAIN_COMPLETION_CONFIG
 
-    user_first_name = update.effective_user.first_name  # TODO oleksandr: update db user info upon every tg update ?
-    tg_update_in_db = UPDATE_DB_MODELS_VOLATILE_CACHE.pop(id(update))
-
-    if user_requested_new_conv:
+    if language_selected:
         # start a new conversation
         await tg_update_in_db.swipy_user.detach_current_conversation()
 
@@ -82,17 +89,33 @@ async def reply_with_gpt_completion(update: Update, context: ContextTypes.DEFAUL
             await asyncio.sleep(10)
 
     main_menu = [
-        [DefaultLang.BTN_I_JUST_WANT_TO_CHAT],
-        [DefaultLang.BTN_SMTH_IS_BOTHERING_ME],
-        [DefaultLang.BTN_HELP_ME_FIGHT_PROCRAST],
-        [DefaultLang.BTN_SOMETHING_ELSE],
+        [lang.BTN_I_JUST_WANT_TO_CHAT],
+        [lang.BTN_SMTH_IS_BOTHERING_ME],
+        [lang.BTN_HELP_ME_FIGHT_PROCRAST],
+        [lang.BTN_SOMETHING_ELSE],
+        [lang.BTN_LANGUAGE],
     ]
 
-    if user_requested_new_conv:
+    if start_requested:
         gpt_completion_in_db = None
 
         response_msg = await update.effective_chat.send_message(
-            text=DefaultLang.MSG_START_TEMPLATE.format(
+            text=lang.MSG_CHOOSE_LANGUAGE,
+            reply_markup=ReplyKeyboardMarkup(
+                [
+                    [lang.BTN_ENGLISH],
+                    [lang.BTN_UKRAINIAN],
+                ],
+                resize_keyboard=True,
+                one_time_keyboard=True,
+            ),
+        )
+
+    elif language_selected:
+        gpt_completion_in_db = None
+
+        response_msg = await update.effective_chat.send_message(
+            text=lang.MSG_START_TEMPLATE.format(
                 USER=user_first_name,
                 BOT=gpt_completion_settings.prompt_settings.bot_name,
             ),
@@ -107,7 +130,7 @@ async def reply_with_gpt_completion(update: Update, context: ContextTypes.DEFAUL
         gpt_completion_in_db = None
 
         response_msg = await update.effective_chat.send_message(
-            text=DefaultLang.MSG_MAIN_MENU,
+            text=lang.MSG_MAIN_MENU,
             reply_markup=ReplyKeyboardMarkup(
                 main_menu,
                 resize_keyboard=True,
@@ -132,14 +155,14 @@ async def reply_with_gpt_completion(update: Update, context: ContextTypes.DEFAUL
 
         buttons = []
         if not any_reply_button_was_pressed:
-            buttons.append([DefaultLang.BTN_EXPAND_ON_THIS])
+            buttons.append([lang.BTN_EXPAND_ON_THIS])
         elif expand_on_this_was_requested:
-            buttons.append([DefaultLang.BTN_THANKS])
-            buttons.append([DefaultLang.BTN_NOT_HELPFUL])
+            buttons.append([lang.BTN_THANKS])
+            buttons.append([lang.BTN_NOT_HELPFUL])
         elif thanks_was_requested:
-            buttons.append([DefaultLang.BTN_PROCEED_WITH_SUBJECT])
+            buttons.append([lang.BTN_PROCEED_WITH_SUBJECT])
         if not expand_on_this_was_requested:
-            buttons.append([DefaultLang.BTN_MAIN_MENU])
+            buttons.append([lang.BTN_MAIN_MENU])
 
         response_msg = await update.effective_chat.send_message(
             text=response_text,
--- swipy_app/swipy_l10n.py
+++ swipy_app/swipy_l10n.py
@@ -11,6 +11,7 @@ class SwipyL10n:
     BTN_SMTH_IS_BOTHERING_ME: str = "Something’s bothering me 😔"
     BTN_HELP_ME_FIGHT_PROCRAST: str = "Help me fight procrastination ✅"
     BTN_SOMETHING_ELSE: str = "Something else 🤔"
+    BTN_LANGUAGE: str = "Language / Мова 🌐"
     BTN_MAIN_MENU: str = "Main menu 🏠"
     BTN_EXPAND_ON_THIS: str = "Expand on this 📚"
     BTN_THANKS: str = "Thanks 🌟"
@@ -20,21 +21,26 @@ class SwipyL10n:
     MSG_START_TEMPLATE: str = "Hi {USER}! My name is {BOT}🤖\n\nHow can I help you? 😊"
     MSG_MAIN_MENU: str = "Sure, what would you like?"
 
+    MSG_CHOOSE_LANGUAGE: str = "Choose your language / Оберіть мову 🌐"
+    BTN_ENGLISH: str = "English 🇬🇧"
+    BTN_UKRAINIAN: str = "Українська 🇺🇦"
 
-SwipyEng = SwipyL10n()
-SwipyUkr = SwipyL10n(
-    MAX_CONVERSATION_LENGTH=10,
-    BTN_I_JUST_WANT_TO_CHAT="Хочу просто поговорити 😊",
-    BTN_SMTH_IS_BOTHERING_ME="Мене щось турбує 😔",
-    BTN_HELP_ME_FIGHT_PROCRAST="Допоможи з прокрастинацією ✅",
-    BTN_SOMETHING_ELSE="Щось інше 🤔",
-    BTN_MAIN_MENU="Головне меню 🏠",
-    BTN_EXPAND_ON_THIS="Детальніше 📚",
-    BTN_THANKS="Дякую 🌟",
-    BTN_NOT_HELPFUL="Не допомогло 💔",
-    BTN_PROCEED_WITH_SUBJECT="Продовжуймо цю тему 📚",
-    MSG_START_TEMPLATE="Привіт {USER}! Мене звати {BOT}🤖\n\nЯк я можу тобі допомогти? 😊",
-    MSG_MAIN_MENU="Звичайно, що тобі потрібно?",
-)
 
-DefaultLang = SwipyUkr
+LANGUAGES = {
+    "en": SwipyL10n(),
+    "uk": SwipyL10n(
+        MAX_CONVERSATION_LENGTH=10,
+        BTN_I_JUST_WANT_TO_CHAT="Хочу просто поговорити 😊",
+        BTN_SMTH_IS_BOTHERING_ME="Мене щось турбує 😔",
+        BTN_HELP_ME_FIGHT_PROCRAST="Допоможи з прокрастинацією ✅",
+        BTN_SOMETHING_ELSE="Щось інше 🤔",
+        BTN_MAIN_MENU="Головне меню 🏠",
+        BTN_EXPAND_ON_THIS="Детальніше 📚",
+        BTN_THANKS="Дякую 🌟",
+        BTN_NOT_HELPFUL="Не допомогло 💔",
+        BTN_PROCEED_WITH_SUBJECT="Продовжуймо цю тему 📚",
+        MSG_START_TEMPLATE="Привіт {USER}! Мене звати {BOT}🤖\n\nЯк я можу тобі допомогти? 😊",
+        MSG_MAIN_MENU="Звичайно, що тебе цікавить?",
+    ),
+}
+DEFAULT_LANG = LANGUAGES["en"]
